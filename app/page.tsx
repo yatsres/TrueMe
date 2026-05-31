69a29e9269e24461d0ee07c0b7ae5aa86b89706f
yatsres <yatsres@gmail.com>
2026-05-31 13:58:11 +0000

    Remove unused imports from page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,3 @@
-import Header from '@/components/Header';
-import Hero from '@/components/Hero';
-import CardGrid from '@/components/CardGrid';
-import AboutSection from '@/components/AboutSection';
-import StorytellingSection from '@/components/StorytellingSection';
-import AboutSection023 from '@/components/AboutSection023';
-import WhatSection030 from '@/components/WhatSection030';
-import WhatTeensSection from '@/components/WhatTeensSection';
-import MethodsSection from '@/components/MethodsSection';
-import ResultsSection from '@/components/ResultsSection';
-import FacilitatorsSection from '@/components/FacilitatorsSection';
-import VideoSection from '@/components/VideoSection';
-import LocationSection from '@/components/LocationSection';
-import PriceSection from '@/components/PriceSection';
-import Footer from '@/components/Footer';
 import PageContent from '@/components/PageContent';
 
 export default function Home() {
